ebe7b469bb4fc3b342a9aae14fd1e4a3d69e1cc7
Vova Mitsura <vovamitsura@gmail.com>
2024-05-02 02:38:27 +0300

    Chore: initial commit

---
+++ src/main/java/org/example/internship_task_2/InternshipTask2Application.java
@@ -0,0 +1,13 @@
+package org.example.internship_task_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipTask2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/internship_task_2/InternshipTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.internship_task_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
